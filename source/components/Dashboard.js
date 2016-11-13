978e8fc4394f26111bca25b68560da6f47643eba
Sergey Rubets <srubets@speroteck.com>
2016-11-13 20:32:48 +0200

    Example of best hero was added

--- source/components/Dashboard.js
+++ source/components/Dashboard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
 
 import HeroesList from './HeroesList';
 
@@ -13,14 +12,19 @@ export default class Dashboard extends Component {
         <h2>Top Heroes</h2>
         <HeroesList heroes={heroes} classView="dashboard" />
         {this.props.children}
-        <Link className="button" to="new-hero">Add new Hero</Link>
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
+  // Some logic to select the best heroes
+  // and return it, for example:
+  // ---------
+  const bestHeroes = state.length > 5 ? state.slice(0, 5) : state.slice(0);
+  // ---------
+
   return {
-    heroes: state
+    heroes: bestHeroes
   }
 }
